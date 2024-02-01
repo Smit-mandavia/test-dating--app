e94b8291681706667ae362e7055c496814c08b05
Smit Mandavia <smitmandavia2@gmail.com>
2024-02-01 15:28:51 +0000

    fix 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = 'mongodb://localhost:27017';
 
 app.use(cors());
 app.use(express.json());
